ec07119eae7254572f0ad6c1ac6487a889f11702
jeong-sik <yousleepwhen@gmail.com>
2026-04-22 12:09:18 +0900

    build: bump oas version to 0.170.3

--- lib/sdk_version.ml
+++ lib/sdk_version.ml
@@ -1,5 +1,5 @@
 (** Single source of truth for the SDK version string.
     All other modules reference this instead of hardcoding. *)
 
-let version = "0.170.2"
+let version = "0.170.3"
 let sdk_name = "agent_sdk"
