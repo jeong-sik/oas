4fd8280c000fcb661bcf823f7617a38001310c4c
Jeong-Sik Yun <45959991+jeong-sik@users.noreply.github.com>
2026-04-30 21:36:23 +0900

    chore(release): bump version to 0.187.3 (#1274)

--- lib/sdk_version.ml
+++ lib/sdk_version.ml
@@ -1,5 +1,5 @@
 (** Single source of truth for the SDK version string.
     All other modules reference this instead of hardcoding. *)
 
-let version = "0.187.2"
+let version = "0.187.3"
 let sdk_name = "agent_sdk"
