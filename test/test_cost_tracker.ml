966fc2874796b68971b07ff6fa632d73b9eae43a
jeong-sik <yousleepwhen@gmail.com>
2026-07-04 20:43:04 +0900

    chore: restore ocamlformat on main

--- test/test_cost_tracker.ml
+++ test/test_cost_tracker.ml
@@ -40,7 +40,14 @@ let test_report_basic () =
 
 let test_report_cache_miss_input_tokens () =
   let usage =
-    make_usage ~cost:0.05 ~calls:2 ~inp:1000 ~out:100 ~cache_creation:150 ~cache_read:650 ()
+    make_usage
+      ~cost:0.05
+      ~calls:2
+      ~inp:1000
+      ~out:100
+      ~cache_creation:150
+      ~cache_read:650
+      ()
   in
   let r = Cost_tracker.report usage in
   check int "cache write" 150 r.cache_creation_tokens;
@@ -55,9 +62,7 @@ let test_report_zero_calls () =
 ;;
 
 let test_report_to_string () =
-  let usage =
-    make_usage ~cost:0.123456 ~calls:5 ~inp:500 ~out:200 ~cache_read:300 ()
-  in
+  let usage = make_usage ~cost:0.123456 ~calls:5 ~inp:500 ~out:200 ~cache_read:300 () in
   let r = Cost_tracker.report usage in
   let s = Cost_tracker.report_to_string r in
   check
@@ -70,8 +75,7 @@ let test_report_to_string () =
        let _ = Str.search_forward (Str.regexp_string "0.123456") s 0 in
        true
      with
-     | Not_found -> false)
-  ;
+     | Not_found -> false);
   check
     bool
     "contains cache miss"
