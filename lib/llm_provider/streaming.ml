777e42afb1a5d1b08d726074f1abd078e56bdfcc
jeong-sik <yousleepwhen@gmail.com>
2026-08-04 10:21:58 +0900

    fix(streaming): make Gemini payload cardinality exhaustive

--- lib/llm_provider/streaming.ml
+++ lib/llm_provider/streaming.ml
@@ -2095,7 +2095,7 @@ let validate_gemini_part ~position part =
          "%s.inlineData:not_object(got %s)"
          path
          (Json_util.json_type_name value))
-  | _ -> Error (path ^ ":multiple_payloads")
+  | _ :: _ :: _ -> Error (path ^ ":multiple_payloads")
 ;;
 
 type gemini_payload_failure =
